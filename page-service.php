a00976f1f2350012e9ee30c96ccb2a883a8454fe
kazuki <sbr0913@gmail.com>
2019-01-16 13:28:49 +0900

    adjusted padding

--- page-service.php
+++ page-service.php
@@ -20,7 +20,7 @@
             <h2 class="renovation_title-large">RENOVATION</h2>
             <P class="renovation_title-small">リノベーション事業</P>
           </div>
-          <p class="servicePageItem_txt">
+          <p class="servicePageItem_txt renovation_txt">
             新しい価値を備えた不動産にするために、私たちは空間デザインにこだわったリノベーションを行っています。
             お客様に求められていること、対象となる不動産の状況をイメージすることからリノベーションははじまります。
             壁材、床材、キッチンなどあらゆる素材にもこだわり、お客様に求められる不動産にするために決して妥協せず、マッチングできる上質な空間デザインを実現します。
@@ -35,7 +35,7 @@
             <h2 class="propertySales_title-large">PROPERTY SALES</h2>
             <P class="propertySales_title-small">不動産買取・販売事業</P>
           </div>
-          <p class="servicePageItem_txt">
+          <p class="servicePageItem_txt propertySales_txt">
             売主さまの「空き物件の利用方法」、「現金化への悩み」、「相続対策で物件の処理に困っている」といった悩み。
             買主さまの「より素敵な部屋に住みたい」、「人とは違う部屋がいい」といったニーズ。
             不動産売買においてお客さまごとにそれぞれの悩みやニーズがあります。
